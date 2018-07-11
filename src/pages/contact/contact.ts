21442f56000f707dcad3c2fabc4add8867562693
Julian Aguilar2 <julianaguilar2@guest-wireless-upc-1604-10-120-076-043.usc.edu>
2018-07-11 09:56:26 -0700

    first commit

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -4,11 +4,40 @@ import { NavController } from 'ionic-angular';
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
+  
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  searchQuery: string = '';
+  items: string[];
 
+  constructor(public navCtrl: NavController) {
+    this.initializeItems();
+
+  }
+
+  initializeItems() {
+    this.items = [
+      'TxT',
+      'Non-profit',
+      'Another Non-profit',
+      'One more Non-profit'
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
